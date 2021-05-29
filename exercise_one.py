2afe4ed3217387c801276ea504ae4bf8db750040
Can Seran AYANA <csayanadesign@gmail.com>
2021-05-29 17:05:41 +0300

    Create exercise_one.py
    Exercise One

---
+++ exercise_one.py
@@ -0,0 +1,36 @@
+""" <---- print("Hello World") ----> """
+print("Hello world")
+
+print("Hello ")
+print("world")
+
+print("Hello ", end='')
+print("world")
+
+""" <---- #comment ----> """
+print("Hello world")  # the comment STARTS at the hash symbol
+# print("Hello world");
+
+'''
+This is a multi-line comment
+You can then easily end the comment with a triple quote (see below)
+'''
+
+# You can also do a multi-line comment
+# like this!
+
+""" <---- varible = "var" ----> """
+
+variable_ex = "Hello world"
+print(variable_ex)
+
+"""
+correct = variable, _variable, var_i_able, var1able
+incorrect = 1variable, var-i-able, var!able, class
+case sensitive/all different variables = variable, Variable, VARIABLE
+"""
+
+string_variable = "This is a string"
+boolean_variable = True  # False
+integer_variable = 500
+float_variable = 50.0
